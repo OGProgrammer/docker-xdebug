ca9734b2cb546a8393d295e9126a514413c92c6f
Joshua Copeland <joshuarcopeland@gmail.com>
2017-08-05 09:08:54 -0700

    Js setup explanation

--- code/example/web/debugme.js
+++ code/example/web/debugme.js
@@ -1,4 +1,10 @@
-// A simple js file for debug example
+/** A simple js file for debug example **/
+
+// To add a JS debug config:
+//   Click down arrow in phpstorm, edit config, + sign and add javascript debug,
+//   Add example.local as url and in url mapped to code/example/web directory
+//   Click save and then click the bug uptop to start the js debug session
+//   * Make sure you have the jetbrains chrome extension installed!
 var msg = "Now that";
 
 document.getElementById("demo").innerHTML = msg;
